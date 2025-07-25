9046fb8683120ef686a0200ba1da03554f69726e
AniketKSingh <aniket20092001@gmail.com>
2025-07-25 06:20:28 +0530

    Lecture_1

---
+++ Lecture_1/index.js
@@ -0,0 +1,24 @@
+
+console.log("Hi Buddy");
+let age = 24
+console.log(age);
+var a = 12
+var a = 14
+console.log(a);
+
+const student = {
+    fullName : "Rahul Kumar",
+    age : 24,
+    cgpa : 8.2,
+    isPass: true
+
+};
+
+console.log(typeof student);
+
+const product = {
+    title : "Parker ball Pen",
+    rating: 4,
+    offer: 5,
+    Price: 270
+};
