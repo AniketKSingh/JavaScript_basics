0bb7234be8cc57402a8b5dc7ade37f88c0aa84e4
AniketKSingh <aniket20092001@gmail.com>
2025-08-04 05:20:49 +0530

    Lecture 4 - arrays

---
+++ Lecture_4/script.js
@@ -0,0 +1,127 @@
+// strings and loops
+// let sum = 0;
+// for(let i= 1; i <= 5; i++){
+//     sum += i;
+// }
+
+// console.log("Sum = ", sum);
+
+// let j = 1;
+
+// while(j <= 5){
+//     console.log(j);
+//     j++;
+// }
+
+// let i = 1;
+
+// do{
+//     console.log(i);
+//     i++;
+// }while(i <= 5);
+
+// let str = "ApnaCollege";
+
+// for(let val of str){
+//     console.log(val);
+// }
+
+// let student = {
+//     name: "Chanchal",
+//     age : 24,
+//     cgpa : 8.0,
+//     isPass : true
+// };
+
+// console.log(student);
+
+// for(let i in student){
+//     console.log(i, ":", student[i]);
+// }
+
+// let sentence = `This is tempelate ${1+2+3}`;
+// console.log(sentence);
+
+// let str = "ApnaCollege";
+// console.log(str.toUpperCase());
+// console.log(str.toLowerCase());
+
+// let s = "    HPC   I&E  ";
+// console.log(s.trim());
+
+
+//To get a part of string, the ending is not inclusive
+// let str = "ApnaCollege";
+// console.log(str.slice(0,4));
+
+// strings are immutable -> that is it cannot be changed 
+
+// let str = "apna";
+// let str2 = "college";
+// console.log(str + str2);
+// let newStr = str.concat(str2);
+// console.log(newStr);
+
+// let s = "hello";
+// let ns = s.replace("lo","p");
+// console.log(ns);
+
+// Arrays
+
+// let heroes = ["ironman", "superman", "Batman", "Hulk", "Thor"];
+
+// for(let i = 0; i < heroes.length; i++){
+//     console.log(heroes[i]);
+// }
+
+// for(let val of heroes){
+//     console.log(val);
+
+// }
+
+// let marks = [88, 62, 29, 90, 99, 42];
+
+// let sum = 0;
+// for(let m of marks){
+//     sum += m;
+
+// }
+
+// console.log("Avg: ", sum/marks.length);
+
+// let items = [250, 645, 300, 500, 50];
+// console.log(items);
+// for(let i = 0; i < items.length; i++){
+//     items[i] = items[i] - items[i]* 0.1;
+// }
+
+// console.log(items);
+
+// Array Methods
+let arr = [10, 22, 6, 88];
+console.log(arr);
+
+// arr.push(77);
+// console.log(arr);
+
+// arr.pop();
+// console.log(arr);
+
+// arr.unshift(77);
+// console.log(arr);
+
+// arr.shift();
+// console.log(arr);
+
+// console.log(arr.slice(0, 2));
+
+// arr.splice(1, 0, 101, 102, 103, 104);
+// console.log(arr);
+
+// console.log(arr.toString());
+
+let vegies = ["Potato", "apple", "beans", "tomato"];
+console.log(vegies);
+
+console.log(vegies.toString());
+
